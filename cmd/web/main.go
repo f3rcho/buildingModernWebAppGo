3a6a702fee4dd98e2f7d71017f989ae2fdc0b312
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-06-03 09:33:58 -0400

    Setting application wide configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/f3rcho/buildingModernWebAppGo/pkg/config"
 	"github.com/f3rcho/buildingModernWebAppGo/pkg/handlers"
+	"github.com/f3rcho/buildingModernWebAppGo/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
