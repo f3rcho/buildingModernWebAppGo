b55eba449ed80305cdc08c9788a85f2a3e182d7e
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-06-03 08:52:59 -0400

    Working with layouts, andbuilding a tempalte cache 1

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,14 +4,62 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
+//map of functions to use later
+// here we could create some own functions that are not in the built-in.
+var functions = template.FuncMap{}
 
 // RenderTemplate is the handler to render and parse templates
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	_, err := RenderTemplateTest(w)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error rendering template", err)
 		return
 	}
+}
+
+// RenderTemplateTest
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	// Get all the pages. Find them
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("PAge is currently", page)
+		//Templates set
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		// Should I use a layout, then find it
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
